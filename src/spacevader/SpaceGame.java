591acc1fb752ccac08507e4c7558129cfde4f1fb
Gmancer432 <sean@richens.com>
2018-01-10 14:29:49 -0700

    Revert "game logic all done!"
    This reverts commit 70046b9e2ee0f4f18a268ebcf84206f519819e3e.

--- src/spacevader/SpaceGame.java
+++ src/spacevader/SpaceGame.java
@@ -21,14 +21,9 @@ public class SpaceGame extends JFrame{
     private boolean spacePressed = false;
     private final int speed = 20;
 
-    private final int row = 10;
-    private final int column = 4;
-    private int alienTime = 6;
+    private int alienTime = 5;
     private int alienMove = alienTime;
     private int alienPos = 0; //0-8, controls where aliens move to next
-    private int aliensLeft;
-
-    private int score = 0;
 
     public SpaceGame()
     {
@@ -38,11 +33,11 @@ public class SpaceGame extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
+        int row = 10; //number of aliens per row
         int s = (getWidth()-row*50)/(row+1); //space between aliens
         int w = 50 + s; //width of aliens + space
         int h = 50 + 30; //vertical spacing
-        fleet = new Alien[row][column];
-        aliensLeft = row*column;
+        fleet = new Alien[row][4];
         for(int j = 0; j<fleet[0].length; j++) {
             for (int i = 0; i < row; i++) {
                 fleet[i][j] = new Alien(i * w + s, 50+j*h);
@@ -78,7 +73,6 @@ public class SpaceGame extends JFrame{
                             Bullet b = ship.shoot(true);
                             bullets.add(b);
                             add(b);
-                            score-=100;
                         }
                         break;
                 }
@@ -109,102 +103,75 @@ public class SpaceGame extends JFrame{
     private void checkCollision(Bullet b) {
         Rectangle brec = new Rectangle(b.getX(), b.getY(), b.getWidth(), b.getHeight());
         Rectangle arec;
-        if(b.isUp()) {//collision for bullet v alien
+        if(b.isUp()) {
             for (Alien[] ar : fleet) {
                 for (Alien a : ar) {
                     if (a != null) {
                         arec = new Rectangle(a.getX(), a.getY(), a.getWidth(), a.getHeight());
                         if (arec.intersects(brec)) {
-                            score+=500;
-                            aliensLeft--;
                             remove(a);
                             ar[Arrays.asList(ar).indexOf(a)] = null;
                             remove(b);
                             bullets.remove(b);
-                            //checks if any aliens are left, and if not, rewards score
-                            if(aliensLeft==0)
-                            {
-                                score+=5000;
-                                t.cancel();
-                                repaint();
-                                newRound();
-                            }
                             break;
                         }
                     }
                 }
             }
         }else
-        {//collision for bullet v player
+        {
             arec = new Rectangle(ship.getX(), ship.getY(), ship.getWidth(), ship.getHeight());
             if (arec.intersects(brec)) {
-                gameOver();
+                dispose();
             }
         }
     }
 
     private void moveFleet()
     {
-        int dir;//0=down, 1=left, 2=right
+        boolean left;
         switch (alienPos) {
             case 0:
-                dir = 2;
+                left = false;
                 break;
             case 1:
-                dir = 2;
+                left = false;
                 break;
             case 2:
-                dir = 2;
+                left = true;
                 break;
             case 3:
-                dir = 0;
+                left = true;
                 break;
             case 4:
-                dir = 1;
+                left = true;
                 break;
             case 5:
-                dir = 1;
+                left = true;
                 break;
             case 6:
-                dir = 1;
+                left = false;
                 break;
             case 7:
-                dir = 1;
-                break;
-            case 8:
-                dir = 1;
-                break;
-            case 9:
-                dir = 0;
-                break;
-            case 10:
-                dir = 2;
-                break;
-            case 11:
-                dir = 2;
+                left = false;
                 break;
             default:
-                dir = 0;
+                left = false;
                 break;
         }
         alienPos++;
-        alienPos%=12 ;
-        alienFlight:
+        alienPos%=9;
         for (Alien[] ar : fleet)
         {
             for (Alien a : ar) {
                 if(a!=null) {
-                    if(dir==0) {
+                    if(alienPos==0) {
                         a.shift(20, 0);
-                        if((a.getY()+50)>=ship.getY())//checks if aliens got too far down
-                        {
-                            gameOver();
-                            break alienFlight;
-                        }
                     }else
                     {
-                        a.shift(0, (dir==1)? -20 : 20);
+                        a.shift(0, (left)? -20 : 20);
                     }
+
                     //also here's the code for the aliens to shoot
                     java.util.Random gen = new Random();
                     if(gen.nextInt(100)==0) {
@@ -215,45 +182,6 @@ public class SpaceGame extends JFrame{
                 }
             }
         }
-    }
-
-    private void newRound()
-    {
-        try {//pause
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        //refill aliens
-        int s = (getWidth()-row*50)/(row+1); //space between aliens
-        int w = 50 + s; //width of aliens + space
-        int h = 50 + 30; //vertical spacing
-        for(int j = 0; j<fleet[0].length; j++) {
-            for (int i = 0; i < row; i++) {
-                fleet[i][j] = new Alien(i * w + s, 50+j*h);
-                add(fleet[i][j]);
-            }
-        }
-        aliensLeft=row*column;
-        repaint();
-        //shorten time for aliens to shift
-        if(alienTime!=1)
-            alienTime--;
-        //pause
-        try {//pause
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        t = new Timer();
-        t.schedule(new MyTimerTask(), 0, 1000/fps);
-    }
-
-    private void gameOver()
-    {
-        t.cancel();
-        System.out.print(score);
-        dispose();
     }
 
     private void act()
@@ -273,7 +201,6 @@ public class SpaceGame extends JFrame{
         alienMove--;
         if(alienMove==0)
         {
-            score++;
             alienMove = alienTime;
             moveFleet();
         }
